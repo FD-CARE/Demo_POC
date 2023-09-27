cfb2039c67af8c56df95dd98ec63105efc2c5ad5
Banik <chiradeep13@gmail.com>
2023-09-27 23:46:27 +0530

    [DeviceMotion] Added the sensor data using this

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,6 @@
 let container = document.getElementById("container");
 
-container.innerHTML="<h1>notFound</h1>"
-
 if ('DeviceOrientationEvent' in window) {
-    container.innerHTML="<h1>Found</h1>";
     window.addEventListener("deviceorientation",(event)=>{
         // Gyroscope data is available in event object
         const alpha = event.alpha; // Z-axis rotation
@@ -11,13 +8,58 @@ if ('DeviceOrientationEvent' in window) {
         const gamma = event.gamma; // Y-axis rotation
         console.log(`alpha: ${alpha}, beta: ${beta}, gamma: ${gamma}`);
 
-        container.innerHTML=`
-                <h1>Gyroscopic Data</h1>
+        const dev = document.getElementById("deviceOrientation");
+        if(dev){
+            dev.innerHTML=`
+            <br>
+            <h1>Device Orientation Data</h1>
+            <h1>alpha = ${alpha}</h1>
+            <h1>beta = ${beta}</h1>
+            <h1>gamma = ${gamma}</h1>
+        `;
+        }else{
+            const deviceOrientation = document.createElement("div");
+            deviceOrientation.id="deviceOrientation";
+            deviceOrientation.innerHTML=`
+                <br>
+                <h1>Device Orientation Data</h1>
                 <h1>alpha = ${alpha}</h1>
                 <h1>beta = ${beta}</h1>
                 <h1>gamma = ${gamma}</h1>
-        `
+            `;
+
+            container.appendChild(deviceOrientation);
+        }
+
     });
 }else{
     console.log('Device Orientation API is not supported in this browser.');
 }
+
+window.addEventListener('devicemotion', (event)=>{
+    const acc = event.acceleration;
+    const accWithGravity = event.accelerationIncludingGravity;
+    const rotationRate = event.rotationRate;
+
+    const dev = document.getElementById("deviceMotion");
+    if(dev){
+        dev.innerHTML=`
+            <br>
+            <h1>Device Motion Data</h1>
+            <h1>acc = { x : ${acc.x} , y : ${acc.y} , z : ${acc.z} }</h1>
+            <h1>accWithGravity = { x : ${accWithGravity.x} , y : ${accWithGravity.y} , z : ${accWithGravity.z} }</h1>
+            <h1>rotationRate = { alpha : ${rotationRate.alpha} , beta : ${rotationRate.beta} , gamma : ${rotationRate.gamma} }</h1>
+        `;
+    }else{
+        const deviceMotion = document.createElement("div");
+        deviceMotion.id="deviceMotion";
+        deviceMotion.innerHTML=`
+                <br>
+                <h1>Device Motion Data</h1>
+                <h1>acc = { x : ${acc.x} , y : ${acc.y} , z : ${acc.z} }</h1>
+                <h1>accWithGravity = { x : ${accWithGravity.x} , y : ${accWithGravity.y} , z : ${accWithGravity.z} }</h1>
+                <h1>rotationRate = { alpha : ${rotationRate.alpha} , beta : ${rotationRate.beta} , gamma : ${rotationRate.gamma} }</h1>
+        `;
+        container.appendChild(deviceMotion);
+    }
+});
